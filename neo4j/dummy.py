c1caf95a4e8dfeddc2bb95ba1df05c8e40059e9e
Sandy <7pandeys@gmail.com>
2025-07-06 23:36:22 +0530

    Update CI.yml, run pytest

--- neo4j/dummy.py
+++ neo4j/dummy.py
@@ -1,5 +1,5 @@
 def output():
     return "Neo4j"
 
-if __name__ == "__main__":
-    output()
+# if __name__ == "__main__":
+#     output()
--- tests/dummy_test.py
+++ tests/dummy_test.py
@@ -1,4 +1,6 @@
-from Neo4j.neo4j.dummy import output
+# from neo4j.neo4j.dummy import output
+
+from dummy import output
 
 
 def test_dummy():
